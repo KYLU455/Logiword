1e925905b27235d0b23eeefb0f29962bcba01152
KYLU <Kylu455@gmail.com>
2019-10-31 16:57:38 +0100

    game fragment

---
+++ Logiword/app/src/main/java/com/kyluandkylu/android/logiword/GameFragment.java
@@ -0,0 +1,26 @@
+package com.kyluandkylu.android.logiword;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class GameFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_game, container, true);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}

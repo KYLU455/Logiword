be8d33189af3642560826f1cc632d6767f807512
faddik02 <faddik02@gmail.com>
2019-10-31 14:39:11 +0100

    Fragment implementation starts

---
+++ Logiword/app/src/main/java/com/kyluandkylu/android/logiword/ViewModel/MainMenuViewModel.java
@@ -0,0 +1,7 @@
+package com.kyluandkylu.android.logiword.ViewModel;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainMenuViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}

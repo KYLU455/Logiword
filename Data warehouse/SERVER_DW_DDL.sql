7333eff9e5c7577071c8215a5e53c7fc3fd2fb0b
KYLU455 <kylu455@gmail.com>
2019-12-07 12:05:03 +0100

    d friend request

--- Data warehouse/SERVER_DW_DDL.sql
+++ Data warehouse/SERVER_DW_DDL.sql
@@ -5,6 +5,7 @@ drop sequence D_PLAYER_ROW_ID;
 drop sequence D_MULTIPLAYER_GAME_ID;
 drop sequence D_DAILY_CHALLENGE_ID;
 drop sequence D_SINGLE_PLAYER_ID;
+drop sequence D_FRIEND_REQUEST_ID;
 
 drop table F_MULTIPLAYER_GAME_MOVE;
 drop table F_MULTIPLAYER_GAME_RESULTS;
@@ -16,6 +17,7 @@ drop table D_PLAYER;
 drop table D_MULTIPLAYER_GAME;
 drop table D_DAILY_CHALLENGE;
 drop table D_SINGLE_PLAYER_GAME;
+drop table D_FRIEND_REQUEST;
 
 CREATE SEQUENCE D_PLAYER_ROW_ID
     INCREMENT BY 1
@@ -33,18 +35,34 @@ create table D_PLAYER(
     row_id number not null,
     player_id number not null,
     player_name varchar2(32) not null,
-    password VARCHAR2(40) not null,
+    password VARCHAR2(40) not null check (length(password) = 40),
     mail varchar2(32) not null check (mail like '%@%'),
     valid_from date not null,
     valid_to date,
     constraint d_player_id_pk primary key (row_id)
 );
 
+CREATE SEQUENCE D_FRIEND_REQUEST_ID
+    INCREMENT BY 1
+    START WITH 1
+    MINVALUE 1
+    NOMAXVALUE;
+
+create table D_FRIEND_REQUEST(
+    id number not null,
+    status varchar2(32)  check (status in ('ACCEPTED', 'REJECTED') or status is null),
+    valid_from date not null,
+    valid_to date,
+    constraint d_friend_request_id_pk primary key (id)
+);
+
 create table F_FRIEND(
+    friend_request_id number not null,
     player1_id number not null,
     player2_id number not null,
     constraint f_friend_player1_fk_d_player foreign key (player1_id) references D_PLAYER,
-    constraint f_friend_player2_fk_d_player foreign key (player2_id) references D_PLAYER
+    constraint f_friend_player2_fk_d_player foreign key (player2_id) references D_PLAYER,
+    constraint f_friend_friend_request_id_fk_d_friend_request foreign key (friend_request_id) references D_FRIEND_REQUEST
 )pctfree 0;
 
 CREATE SEQUENCE D_DAILY_CHALLENGE_ID

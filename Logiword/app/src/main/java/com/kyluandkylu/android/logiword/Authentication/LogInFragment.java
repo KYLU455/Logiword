16e53b61a6f836cc7aed9bcc4992d84806cdcba0
KYLU455 <kylu455@gmail.com>
2019-12-07 16:37:14 +0100

    support action bar hide

--- Logiword/app/src/main/java/com/kyluandkylu/android/logiword/Authentication/LogInFragment.java
+++ Logiword/app/src/main/java/com/kyluandkylu/android/logiword/Authentication/LogInFragment.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -49,6 +50,7 @@ public class LogInFragment extends Fragment {
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
         logInViewModel = ViewModelProviders.of(this).get(LogInViewModel.class);
 
         editTextMail = view.findViewById(R.id.editTextEmail);
@@ -144,12 +146,18 @@ public class LogInFragment extends Fragment {
     private void moveToMenu(View view) {
         InputMethodManager imm = (InputMethodManager) getContext().getSystemService(getActivity().INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
         getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MainMenu()).commit();
     }
 
     private void pressButton(String buttonText, View view){
         if (logInViewModel.isSecondPress()) {
-            String errorMessage = logInViewModel.logInUser(readUserData(), getContext());
+            String errorMessage;
+            if(buttonText.equals("CREATE ACCOUNT")){
+                errorMessage = logInViewModel.registerUser(readUserData(), getContext());
+            }else {
+                errorMessage = logInViewModel.logInUser(readUserData(), getContext());
+            }
             if (errorMessage.length() > 0) {
                 displayErrorMessage(errorMessage);
             } else {
